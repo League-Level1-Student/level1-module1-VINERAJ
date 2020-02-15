5771aa96f37e695f03594fac59124b4733dd52b8
League Student <league@iMac-21.attlocal.net>
2020-02-15 11:26:14 -0800

    :hurtrealbad: :100:
    On HarryPotter

---
+++ src/Platypus.java
@@ -0,0 +1,12 @@
+
+public class Platypus {
+	private String name;
+	
+	public Platypus(String pname) {
+		name = pname;
+	}
+	
+	void sayHi(){
+        System.out.println("The platypus " + name + " is smarter than your average platypus.");
+    }
+}
